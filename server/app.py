b0234406a1f1e7fedc749ae60ff868b5a74777d8
jimmindungu3 <jimmindungu3@gmail.com>
2024-01-09 13:57:20 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string>')
+def print_string(string):
+    print (string)
+    return f'{string}'
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    numbers = list(range(parameter + 1))
+    return render_template('count.html', numbers=numbers)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
