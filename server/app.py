7e71ba40fa7e41fc1c71176da66afedbaf60e19c
jimmindungu3 <jimmindungu3@gmail.com>
2024-01-10 11:44:03 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
-
-from flask import Flask, render_template, request
+# app.py
+from flask import Flask, render_template, jsonify
+import sys
 
 app = Flask(__name__)
 
@@ -8,15 +8,29 @@ app = Flask(__name__)
 def index():
     return '<h1>Python Operations with Flask Routing and Views</h1>'
 
-@app.route('/print/<string>')
-def print_string(string):
-    print (string)
-    return f'{string}'
+@app.route('/print/<parameter>')
+def print_parameter(parameter):
+    print(parameter)  # Output to console
+    return parameter
 
 @app.route('/count/<int:parameter>')
 def count(parameter):
-    numbers = list(range(parameter + 1))
-    return render_template('count.html', numbers=numbers)
+    return '\n'.join(str(i) for i in range(parameter)) + '\n'
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    result = 0
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    return str(result)
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
